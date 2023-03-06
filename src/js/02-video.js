79500a089ccafa3008db58611996af56140fb4f4
Natalia Grela <nataliagrela83@mail.com>
2023-03-06 17:20:22 +0100

    hw 8
    task 2&3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
-
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player
+      .getCurrentTime()
+      .then(seconds => {
+        localStorage.setItem('videoplayer-current-time', seconds);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, 1000)
+);
+const time = localStorage.getItem('videoplayer-current-time');
+if (time) {
+  player.setCurrentTime(time);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('form');
+const email = document.querySelector('[name="email"]');
+console.log(email);
+const message = document.querySelector('[name="message"]');
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const user = {
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(user));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault(),
+    event.currentTarget.reset(),
+    localStorage.removeItem('feedback-form-state');
+});
